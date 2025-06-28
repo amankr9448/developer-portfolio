76df543fb1cbb724320aefb4a4bb08770833a160
aman <aman>
2025-06-29 01:36:13 +0530

    Info Changes

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,17 +1,26 @@
- export const skillsData = [
-    'HTML',
-    'Fastify',
-    'Blender',
-    'Figma',
-    'Javascript',
-    'CSS',
-    'React',
-    'Django',
-]
+export const skillsData = [
+  // 'HTML',
+  // 'Fastify',
+  // 'Blender',
+  // 'Figma',
+  // 'Javascript',
+  // 'CSS',
+  // 'React',
+  "Docker",
+  "Django",
+  "Python",
+  "PostgreSQL",
+  "AWS",
+  "Azure",
+  "C++",
+  "Graphql",
+  "MySQL",
+  "Nginx",
+  "Tensorflow",
+];
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
 // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-
 
 // AVAILABLE SKILLS
 
